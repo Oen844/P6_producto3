cd35293e52aa2be2f2ce750b385b23fa14d8a99b
Ferran <ferran.puigmar@gmail.com>
2021-05-20 16:18:15 +0200

    Add calendar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -67,11 +67,11 @@
                         {{ config('Porcentaje', 'Porcentaje') }}
                     </a>
 
-                <?php
-               }
-               ?>
-               @endauth
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <?php } ?>
+                @endauth
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
