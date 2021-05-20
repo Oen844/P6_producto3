e9dba35d2b721017e643e82a63edc6b738a8fccc
paucast <papau84@gmail.com>
2021-05-20 22:16:56 +0200

    query

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -22,19 +22,27 @@ class CalendarController extends Controller
 
     public function index()
     {
+        // 'id' => 'reuired',
+        // 'time_start' => 'required',
+        // 'time_end' => 'required',
+        // 'day' => 'required',
+        // 'id_class' => 'required',
 
         $id = Auth::user()->id;
         $user = Auth::user()->tipo;
         if ($user == 2) {
-            // $calendarSchedules = DB::table('schedules')
-            //     ->join('asignaturas', function ($join) {
-            //         $join->on('asignaturas.id_schedule', '=', 'schedules.');
-            //     })
-            //     ->get();
+            $calendarSchedules = DB::table('asignaturas')
+            ->join('enrollments', 'asignaturas.id_course', '=', 'enrollments.id_course')
+            ->join('schedules', 'asignaturas.id_teacher', '=', 'schedules.id_class')
+            ->join('users', 'asignaturas.id', '=', 'users.id')
+            ->where('enrollments.id_student','=',Auth::user()->id)
+            ->select('asignaturas.name', 'schedules.time_start', 'schedules.time_end', 'users.name' )
+            ->get();
 
-            $calendarSchedules = "";
 
-            return view('calendar.index', compact('calendarSchedules'));
+
+            //return view('calendar.index', compact('calendarSchedules'));
+            return "$calendarSchedules";
         }
     }
 }
